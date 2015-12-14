1b9ff84fd035c25ae9265f223c56f608ac420ac1
sirinrat3010 <tanlove_3010@gmail.com>
2015-12-14 23:28:48 +0700

    หน้า CalendarDayEvent

--- library/src/main/java/com/tl/calendar/app/CalendarDayEvent.java
+++ library/src/main/java/com/tl/calendar/app/CalendarDayEvent.java
@@ -4,4 +4,50 @@ package com.tl.calendar.app;
  * Created by AMONRATS on 14/12/2558.
  */
 public class CalendarDayEvent {
-}
+
+        private final long timeInMillis;
+        private final int color;
+
+        public CalendarDayEvent(final long timeInMillis, final int color) {
+            this.timeInMillis = timeInMillis;
+            this.color = color;
+        }
+
+        public long getTimeInMillis() {
+            return timeInMillis;
+        }
+
+        public int getColor() {
+            return color;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CalendarDayEvent event = (CalendarDayEvent) o;
+
+            if (color != event.color) return false;
+            if (timeInMillis != event.timeInMillis) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = (int) (timeInMillis ^ (timeInMillis >>> 32));
+            result = 31 * result + color;
+            return result;
+        }
+
+        @Override
+        public String
+        toString() {
+            return "CalendarDayEvent{" +
+                    "timeInMillis=" + timeInMillis +
+                    ", color=" + color +
+                    '}';
+        }
+    }
+
