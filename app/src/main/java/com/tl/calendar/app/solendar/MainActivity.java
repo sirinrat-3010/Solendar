5a461e05198f6a7e2799d031edd90cbc2b6390b0
sirinrat3010 <tanlove_3010@gmail.com>
2015-12-14 22:02:25 +0700

    หน้า MfainActivity

--- app/src/main/java/com/tl/calendar/app/solendar/MainActivity.java
+++ app/src/main/java/com/tl/calendar/app/solendar/MainActivity.java
@@ -1,13 +1,12 @@
 package com.tl.calendar.app.solendar;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-        private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
+    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
 
 }
